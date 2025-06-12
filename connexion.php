e14e6a22f6deb81aa71538a648cc3f24bf89adaa
joslath <ojoslath@gmail.com>
2025-06-12 02:42:35 +0100

    Ajout du Dockerfile, docker-compose et connexion PostgreSQL

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("pgsql:host=db;port=5432;dbname=projet_tuteur", "postgres", "jojo");
+    $pdo = new PDO("pgsql:host=dpg-d152is3uibrs73bgrilg-a.frankfurt-postgres.render.com;port=5432;dbname=projet_tuteur_v9mg", "projet_tuteur_v9mg_user", "i5m26GjTcPQ6C0hSHnkORa4fKYlmEet3");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
